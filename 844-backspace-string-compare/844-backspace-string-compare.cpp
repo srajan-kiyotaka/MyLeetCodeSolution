6aa95b6890598086e713721cc864240215d382bf
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2022-07-23 11:06:39 +0530

    Time: 3 ms (52.03%), Space: 6.5 MB (27.95%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> st1;
+        stack<char> st2;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] != '#'){
+                st1.push(s[i]);
+            }
+            else{
+                if(!st1.empty())
+                    st1.pop();
+            }
+        }
+        
+        for(int i = 0; i < t.length(); i++){
+            if(t[i] != '#'){
+                st2.push(t[i]);
+            }
+            else{
+                if(!st2.empty())
+                    st2.pop();
+            }
+        }
+        
+        if(st1.size() != st2.size()) return false;
+        else{
+            while(!st1.empty()){
+                if(st1.top() != st2.top()){
+                    return false;
+                }
+                st1.pop();
+                st2.pop();
+            }
+            return true;
+        }
+        return true;
+    }
+};
