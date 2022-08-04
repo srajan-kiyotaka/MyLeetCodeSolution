cdf113eef7df07ab4dbf54cbe4ab7dd97e2c4461
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2022-08-04 13:45:16 +0530

    Time: 87 ms (13.24%), Space: 18.1 MB (17.87%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void bfs(vector<vector<char>>& grid, int i, int j){
+        queue<pair<int, int>> q;
+        q.push({i,j});
+        while(!q.empty()){
+            auto t = q.front();
+            q.pop();
+            if(t.first >= grid.size() && t.second >= grid[0].size()) continue;
+            if(grid[t.first][t.second] == 'c') continue;
+            grid[t.first][t.second] = 'c';
+            if(t.second + 1 < grid[0].size() && grid[t.first][t.second + 1] == '1')
+                q.push({t.first, t.second + 1});
+            if(t.first + 1 < grid.size() && grid[t.first + 1][t.second] == '1')
+                q.push({t.first + 1, t.second});
+            if(t.second - 1 >= 0 && grid[t.first][t.second - 1] == '1')
+                q.push({t.first, t.second - 1});
+            if(t.first - 1 >= 0 && grid[t.first - 1][t.second] == '1')
+                q.push({t.first - 1, t.second});
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.empty()) return 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        int island = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1'){
+                    island++;
+                    // cout << island << endl;
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        
+        return island;
+    }
+};
