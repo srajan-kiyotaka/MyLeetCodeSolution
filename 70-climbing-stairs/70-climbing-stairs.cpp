5b494c878f92ba16a0961ee26136e137647e7841
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2022-08-12 15:26:31 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (15.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     unordered_map<int, int> map;
-    int clime(int n){
+    
+    int climbStairs(int n){
         if(n == 0) return 1;
         if(n == 1) return 1;
         if(map.find(n) != map.end()) return map[n];
         int ans = 0;
         
         if(n > 0){
-            ans += clime(n - 1);
-            ans += clime(n - 2);
+            ans += climbStairs(n - 1);
+            ans += climbStairs(n - 2);
         }
         map[n] = ans;
         return ans;
-    }
-    int climbStairs(int n) {
-        return clime(n);
     }
 };
