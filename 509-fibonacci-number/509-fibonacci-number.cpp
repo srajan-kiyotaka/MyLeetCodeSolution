c29b8e6fe12f196df6bf78a1454e405e8f46febe
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2022-08-21 19:59:11 +0530

    Time: 18 ms (25.73%), Space: 5.9 MB (78.79%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
+    int f(int n){
+        if(n < 2) return n;
+        return f(n - 1) + f(n - 2);
+    }
+    
     int fib(int n) {
+        return f(n);
         if(n == 0) return 0;
         if(n == 1) return 1;
         int ans = 1, temp = 0;
