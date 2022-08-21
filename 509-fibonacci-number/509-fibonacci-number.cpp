d02e316d93eb2cd7516e2ca769e6e2a5ac073ccc
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2022-08-21 20:05:52 +0530

    Time: 2 ms (63.29%), Space: 6.4 MB (8.70%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    unordered_map<int, int> map;
     int f(int n){
         if(n < 2) return n;
-        return f(n - 1) + f(n - 2);
+        if(map.find(n) != map.end()) return map[n];
+        map[n] = f(n - 1) + f(n - 2);
+        return map[n];
     }
     
     int fib(int n) {
+        map[0] = 0;
+        map[1] = 1;
+        map[2] = 1;
         return f(n);
         if(n == 0) return 0;
         if(n == 1) return 1;
