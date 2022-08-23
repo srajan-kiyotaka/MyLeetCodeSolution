d9f6d7149505769e27778ff85a332e6528298cf8
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2022-08-24 00:06:33 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (23.43%) - LeetHub

--- 507-perfect-number/507-perfect-number.cpp
+++ 507-perfect-number/507-perfect-number.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    vector<int> findDiv(int num){
-        vector<int> ans;
-        ans.push_back(1);
+    // vector<int> findDiv(int num){
+    //     vector<int> ans;
+    //     ans.push_back(1);
+    //     int temp = 2;
+    //     while(temp <= sqrt(num)){
+    //         if(num % temp == 0){
+    //             if(temp == num/temp) ans.push_back(temp);
+    //             else{
+    //                 ans.push_back(temp);
+    //                 ans.push_back(num/temp);
+    //             }
+    //         }
+    //         temp++;
+    //     }
+    //     return ans;
+    // }
+    
+    bool checkPerfectNumber(int num) {
+        if(num == 1) return false;
+        int ans = 1;
         int temp = 2;
         while(temp <= sqrt(num)){
             if(num % temp == 0){
-                if(temp == num/temp) ans.push_back(temp);
+                if(temp == num/temp) ans += temp;
                 else{
-                    ans.push_back(temp);
-                    ans.push_back(num/temp);
+                    ans += temp;
+                    ans += num/temp;
                 }
             }
             temp++;
         }
-        return ans;
-    }
-    
-    bool checkPerfectNumber(int num) {
-        if(num == 1) return false;
-        vector<int> ans = findDiv(num);
+        
+        // vector<int> ans = findDiv(num);
         // cout << accumulate(ans.begin(), ans.end(), 0) << endl;
-        if(num == accumulate(ans.begin(), ans.end(), 0)) return true;
+        if(num == ans) return true;
         return false;
     }
 };
