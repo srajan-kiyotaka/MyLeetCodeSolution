89246de1cdf8593ebdc1e1241f9738336fdec5f5
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2022-08-28 13:37:30 +0530

    Time: 13 ms (6.96%), Space: 11.3 MB (31.07%) - LeetHub

---
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minNumberOfHours(int initialEnergy, int initialExperience, vector<int>& energy, vector<int>& experience) {
+        // int max_exp = max(experience.begin(), experience.end());
+        int max_exp = *max_element(experience.begin(), experience.end());
+        int sum = accumulate(energy.begin(), energy.end(), 0);
+        int hour = 0;
+        if(sum >= initialEnergy){
+            hour = sum - initialEnergy + 1;  
+        }
+        if(initialExperience <= max_exp){
+            int i = 0;
+            while(i < experience.size() ){// && initialExperience <= max_exp
+                if(experience[i] >= initialExperience){
+                    hour += experience[i] - initialExperience + 1;
+                    initialExperience += 2*experience[i++] - initialExperience + 1;
+                    continue;
+                }
+                initialExperience += experience[i++];
+            }
+        }
+        
+           return hour;
+//         int hour = 0;
+//         for(int i = 0; i < energy.size(); i++){
+//             if(initialEnergy <= energy[i]){
+//                 hour += energy[i] - initialEnergy + 1;
+//             }
+//             initialEnergy -= 
+            
+//             if(initialExperience){
+                
+//             }
+//         }
+    }
+};
