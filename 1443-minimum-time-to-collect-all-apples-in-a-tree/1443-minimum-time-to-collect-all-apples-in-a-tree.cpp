350e5ffb995ff52da8bb6c8259cb0224511a5b5f
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-01-12 01:03:32 +0530

    Time: 195 ms (81.88%), Space: 60.5 MB (64.77%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int ans_fun(int parent, int node, vector<vector<int>>& adj, vector<bool>& hasApple){
+         int total = 0;
+         int child_time = 0;
+         for(auto child: adj[node]){
+             if(child == parent) continue;
+             child_time = ans_fun(node, child, adj, hasApple);
+             if(child_time || hasApple[child]) total += child_time + 2;
+         }
+         return total;
+    }
+
+
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        vector<vector<int>> adj(n, vector<int>(0));
+        int i = 0;
+        while(i < edges.size()){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+            i++;
+        }
+
+        return ans_fun(-1, 0, adj, hasApple);
+    }
+};
