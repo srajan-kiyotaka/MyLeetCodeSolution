815a9d878c4e59b6cad17cad371c696f91da0118
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-01-12 21:49:30 +0530

    Time: 607 ms (82.22%), Space: 219.6 MB (48.15%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-    vector<int> dfs(int node, int parent, vector<vector<int>>& adj, string& labels,
+    vector<int> dfs1(int node, int parent, vector<vector<int>>& adj, string& labels,
+                    vector<int>& ans){
+        vector<int> notesCount(26);
+        notesCount[labels[node] - 'a'] = 1;
+        for(auto& child : adj[node]){
+            if(child == parent) continue;
+            vector<int> childCount = dfs1(child, node, adj, labels, ans);
+            for(int i = 0; i < 26; i++){
+                notesCount[i] += childCount[i];
+            }
+        }
+        ans[node] = notesCount[labels[node] - 'a'];
+        return notesCount;
+    }
+
+    vector<int> dfs2(int node, int parent, vector<vector<int>>& adj, string& labels,
                     vector<int>& ans) {
-        // Store count of all alphabets in the subtree of the node.
         vector<int> nodeCounts(26);
         nodeCounts[labels[node] - 'a'] = 1;
-
         for (auto& child : adj[node]) {
             if (child == parent) {
                 continue;
             }
-            vector<int> childCounts = dfs(child, node, adj, labels, ans);
-            // Add frequencies of the child node in the parent node's frequency array.
+            vector<int> childCounts = dfs2(child, node, adj, labels, ans);
             for (int i = 0; i < 26; i++) {
                 nodeCounts[i] += childCounts[i];
             }
         }
-
         ans[node] = nodeCounts[labels[node] - 'a'];
         return nodeCounts;
     }
-
+    
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-        vector<vector<int>> adj(n);
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        vector<vector<int>> adj(n, vector<int>(0));
+        for(int i = 0; i < n-1; i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
         }
-
-        vector<int> ans(n, 0);
-        dfs(0, -1, adj, labels, ans);
-
+        vector<int> ans(n,0);
+        dfs1(0, -1, adj, labels, ans);
         return ans;
     }
 };
