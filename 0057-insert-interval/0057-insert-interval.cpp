171ae97373d20e3c47d5ec6ae27c28d9804c2981
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-01-16 12:48:52 +0530

    Time: 11 ms (97.45%), Space: 17 MB (78.65%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        if(intervals.empty()){
+            intervals.push_back(newInterval);
+            return intervals;
+        }
+        int i = 0;
+        while(intervals[i][0] < newInterval[1]){
+            i++;
+            if(i == intervals.size()){
+                i--;
+                break;
+            }
+        }
+
+        int j = 0;
+        while(intervals[j][1] < newInterval[0]){
+            j++;
+            if(j == intervals.size()){
+                j--;
+                break;
+            }
+        }
+
+        cout << i << " " << j << endl;
+
+        if(i == j){
+            if(i == intervals.size() - 1){
+                if(intervals[i][1] < newInterval[0]){
+                    intervals.insert(intervals.end(), newInterval);
+                    return intervals;
+                }
+            }
+            if(intervals[i][0] > newInterval[1]){
+                intervals.insert(intervals.begin() + i, newInterval);
+                return intervals;
+            }
+            else{
+                intervals[i][0] = min(intervals[i][0], newInterval[0]);
+                intervals[i][1] = max(intervals[i][1], newInterval[1]);
+                return intervals;
+            }
+        }
+        else{
+            if(newInterval[1] < intervals[i][0]){
+                i--;
+                if(i == j){
+                    if(intervals[i][0] > newInterval[1]){
+                        intervals.insert(intervals.begin() + i, newInterval);
+                        return intervals;
+                    }
+                    else{
+                        intervals[i][0] = min(intervals[i][0], newInterval[0]);
+                        intervals[i][1] = max(intervals[i][1], newInterval[1]);
+                        return intervals;
+                    }
+                }
+                else{
+                    intervals[j][0] = min(intervals[j][0], newInterval[0]);
+                    intervals[j][1] = max(intervals[i][1], newInterval[1]);
+                    intervals.erase(intervals.begin() + j + 1, intervals.begin() + i + 1);
+                    return intervals;
+                }
+            }
+            else{
+                cout << "inside" << endl;
+                intervals[j][0] = min(intervals[j][0], newInterval[0]);
+                intervals[j][1] = max(intervals[i][1], newInterval[1]);
+                intervals.erase(intervals.begin() + j + 1, intervals.begin() + i + 1);
+            }
+        }
+
+        return intervals;
+    }
+};
