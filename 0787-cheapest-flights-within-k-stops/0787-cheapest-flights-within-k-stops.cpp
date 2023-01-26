739a8d3b01c4a149c20296375adc6ec532800200
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-01-26 14:58:27 +0530

    Time: 47 ms (38.67%), Space: 15.8 MB (16.53%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -4,13 +4,16 @@ public:
         vector<int> prices(n, INT_MAX);
         prices[src] = 0;
         for(int i = 0; i <= k; i++){
-            vector<int> tempPrices = prices; 
+            vector<int> tempPrices = prices;
+            bool flag = true;
             for(auto flight: flights){
                 if(prices[flight[0]] == INT_MAX) continue;
                 if(prices[flight[0]] + flight[2] < tempPrices[flight[1]]){
+                    flag = false;
                     tempPrices[flight[1]] = prices[flight[0]] + flight[2];
                 }
             }
+            if(flag) break;
             prices = tempPrices;
         }
         return (prices[dst] == INT_MAX)?(-1):prices[dst];
