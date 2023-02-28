88f8019df1174eb648c7a61b255ef4b1db9cc5cb
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-02-28 16:58:16 +0530

    Time: 0 ms (100.00%), Space: 6 MB (64.79%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,12 +4,22 @@ public:
         if(x == INT_MIN) return 0;
         int ans = 0;
         bool flag = x < 0;
-        x = flag ? -x : x; 
+        x = flag ? -x : x;
         while(x != 0){
             if(ans > INT_MAX / 10) return 0;
-            ans = ans * 10 + x % 10;
+            ans = ans*10 + x%10;
             x /= 10;
         }
         return flag ? -ans : ans;
     }
 };
+// if(x == INT_MIN) return 0;
+//         int ans = 0;
+//         bool flag = x < 0;
+//         x = flag ? -x : x; 
+//         while(x != 0){
+//             if(ans > INT_MAX / 10) return 0;
+//             ans = ans * 10 + x % 10;
+//             x /= 10;
+//         }
+//         return flag ? -ans : ans;
