77c5fd0abe13b7f18648897a61d70a6bf1f68b73
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-02-28 17:19:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (66.66%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -3,13 +3,28 @@ public:
     /* Using Complement Method */
     int bitwiseComplement(int n) {
         if(n == 0) return 1;
+        int gMask = 0;
+        // 1's Compliment
+        int x = ~n;
+        while(n != 0){
+            gMask = gMask << 1;
+            gMask = gMask | 1;
+            n = n >> 1;
+        }
+        return (x & gMask);
+    }
+    
+    /*
+    
+    if(n == 0) return 1;
         int ans = 1;
         while(ans <= n){
             ans *= 2;
         }
         return (ans - n - 1);
-    }
-    
+
+    */
+
     /* Using Mask and Not */
     // int bitwiseComplement(int n) {
     //     if(n == 0) return 1;
