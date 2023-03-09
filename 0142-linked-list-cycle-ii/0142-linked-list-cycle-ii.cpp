b3fcac03b147925b08d4dc52c753a76d37cf2d1d
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-03-09 16:01:02 +0530

    Time: 9 ms (52.86%), Space: 7.6 MB (84.91%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,19 +8,47 @@
  */
 class Solution {
 public:
-    ListNode* travelTree(ListNode* root, unordered_map<ListNode*, int>& hash, int i){
-        if(root == NULL){
-            return NULL;
-        }
-        if(hash.find(root) != hash.end()){
-            return root;
-        }
-        hash[root] = i;
-        return travelTree(root->next, hash, i++);
-    }
-
+/*
+Two Pointer Approch:
+Pointer 1 -> Slow
+Pointer 2 -> Fast
+*/
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode*, int> hashMap;
-        return travelTree(head, hashMap, 0);;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
     }
+    
+
+    /*
+     Hash Map Approch: 
+     Time Complexity: O(n)
+    */
+    // ListNode* travelTree(ListNode* root, unordered_map<ListNode*, int>& hash, int i){
+    //     if(root == NULL){
+    //         return NULL;
+    //     }
+    //     if(hash.find(root) != hash.end()){
+    //         return root;
+    //     }
+    //     hash[root] = i;
+    //     return travelTree(root->next, hash, i++);
+    // }
+
+    // ListNode *detectCycle(ListNode *head) {
+    //     unordered_map<ListNode*, int> hashMap;
+    //     return travelTree(head, hashMap, 0);;
+    // }
 };
