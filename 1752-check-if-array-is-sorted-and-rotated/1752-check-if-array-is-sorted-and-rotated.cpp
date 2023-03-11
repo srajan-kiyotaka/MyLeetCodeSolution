1daccce6100531fded54514c2839f31f6af4c172
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-03-11 11:57:46 +0530

    Time: 6 ms (24.26%), Space: 8.5 MB (60.89%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -3,19 +3,38 @@ public:
     bool check(vector<int>& nums) {
         // Chek for eack rotation possible.
         // Time complexity = O(n^2)
-        for(int r = 0; r < nums.size(); r++){
-            bool flag = true;
-            for(int i = 0; i < nums.size() - 1; i++){
-                if(nums[(i + r) % nums.size()] <= nums[(i + 1 + r) % nums.size()]){
-                    continue;
-                }
-                else{
-                    flag = false;
-                    break;
-                }
+        // Brute Force Solution.
+        // for(int r = 0; r < nums.size(); r++){
+        //     bool flag = true;
+        //     for(int i = 0; i < nums.size() - 1; i++){
+        //         if(nums[(i + r) % nums.size()] <= nums[(i + 1 + r) % nums.size()]){
+        //             continue;
+        //         }
+        //         else{
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag) return true;
+        // }
+        // return false;
+        /*
+        Optimized solution:
+        case 1. 1,2,3,4,5,6 -> sorted and rotated.
+                [         ] -> 1 pair of all sorted.
+                            -> 1 Gaps (6 -> 1) (since it is rotated).
+        case 2. 3,4,5,6,1,2 -> sorted and rotated. 
+                [     ] [ ] -> 2 pair of all sorted. 
+                            -> 1 Gaps.
+        */
+        int gaps = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i - 1] > nums[i]){
+                gaps++;
             }
-            if(flag) return true;
         }
+        if(nums[nums.size() - 1] > nums[0]) gaps++;
+        if(gaps <= 1) return true;
         return false;
     }
 };
