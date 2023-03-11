17413565424724982b71bef25e20691ba82eb242
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-03-11 19:39:35 +0530

    Time: 5 ms (30.77%), Space: 6.9 MB (93.61%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>> temp = matrix;
+        // Using Maths
+        // Time Complexity : O(N^2)
+        // Space Complexity : O(1)
+        reverse(matrix.begin(), matrix.end());
         int n = matrix.size();
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                matrix[i][j] = temp[n - j - 1][i];
+            for(int j = i + 1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        temp.erase(temp.begin(), temp.end());
     }
 };
+    
+/*
+// Extra Space Solution!
+    vector<vector<int>> temp = matrix;
+    int n = matrix.size();
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            matrix[i][j] = temp[n - j - 1][i];
+        }
+    }
+    temp.erase(temp.begin(), temp.end());
+
+*/
