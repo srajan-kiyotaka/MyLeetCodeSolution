1f55f9d1f8f1aaf03681103027222dcd2eb955b4
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-03-11 11:37:49 +0530

    Time: 40 ms (20.17%), Space: 25.7 MB (12.46%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,8 +5,9 @@ public:
         for(int i = 0; i < nums.size(); i++){
             arr[(i + k) % nums.size()] = nums[i];
         }
-        for(int i = 0; i < nums.size(); i++){
-            nums[i] = arr[i];
-        }
+        nums = arr;
+        // for(int i = 0; i < nums.size(); i++){
+        //     nums[i] = arr[i];
+        // }
     }
 };
