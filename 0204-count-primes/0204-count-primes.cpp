cc02b6c05898feb13a9e2491c71ced0ee333eb6b
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-03-12 11:10:23 +0530

    Time: 247 ms (41.62%), Space: 10.2 MB (93.79%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        // Using Sieve of Eratosthenes Method
+        vector<bool> numbers(n, true);
+        int ans = 0;
+        for(int i = 2; i < n; i++){
+            if(numbers[i]){
+                ans++;
+                int j = i*2;
+                while(j < n){
+                    numbers[j] = false;
+                    j += i;
+                }
+            }
+        }
+        return ans; 
+    }
+};
