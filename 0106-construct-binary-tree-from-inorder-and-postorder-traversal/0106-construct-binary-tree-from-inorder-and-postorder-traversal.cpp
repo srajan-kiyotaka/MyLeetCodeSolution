9030c39beae05254febeba06952735d50ae6eb7f
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-03-16 13:53:12 +0530

    Time: 28 ms (41.33%), Space: 26.5 MB (48.48%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,14 +11,14 @@
  */
 class Solution {
 public:
-    int search(vector<int>& inorder, int target){
-        for(int i = 0; i < inorder.size(); i++){
-            if(inorder[i] == target) return i;
-        }
-        return -1;
-    }
+    // int search(vector<int>& inorder, int target){
+    //     for(int i = 0; i < inorder.size(); i++){
+    //         if(inorder[i] == target) return i;
+    //     }
+    //     return -1;
+    // }
 
-    TreeNode* createTree(vector<int>& inorder, vector<int>& postorder, int s, int e){
+    TreeNode* createTree(vector<int>& inorder, vector<int>& postorder, int s, int e, unordered_map<int, int>& hash){
         if(s < 0 || s > e) return NULL;
         if(s == e){
             TreeNode* node = new TreeNode(inorder[s]);
@@ -26,16 +26,20 @@ public:
             return node;
         }
         int it = postorder.back();
-        int m = search(inorder, it);
+        int m = hash[it];
         TreeNode* node = new TreeNode(it);
         postorder.pop_back();
-        node->right = createTree(inorder, postorder, m+1, e);
-        node->left = createTree(inorder, postorder, s, m-1);
+        node->right = createTree(inorder, postorder, m+1, e, hash);
+        node->left = createTree(inorder, postorder, s, m-1, hash);
         return node;
     }
 
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         if(inorder.size() < 1) return NULL;
-        return createTree(inorder, postorder, 0, inorder.size() - 1);
+        unordered_map<int, int> hash;
+        for(int i = 0; i < inorder.size(); i++){
+            hash[inorder[i]] = i;
+        }
+        return createTree(inorder, postorder, 0, inorder.size() - 1, hash);
     }
 };
