40524886fcf7c9715084a04a6fce5594f45cccfb
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-03-21 09:38:35 +0530

    Time: 173 ms (61.99%), Space: 107.5 MB (54.45%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,7 +1,70 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans = 0, numSubarray = 0;
+        
+        // Iterate over nums, if num = 0, it has 1 more zero-filled subarray
+        // than the previous one, otherwise, it has 0 zero-filled subarray.
+        for (auto num : nums) {
+            if (num == 0)
+                numSubarray++;
+            else
+                numSubarray = 0;
+            ans += numSubarray;
+        }
+        
+        return ans;
+    }
+};
+
+// 2 Pointer Approch
+/*
+
+long long zeroFilledSubarray(vector<int>& nums) {
         long long ans = 0;
+        int i = 0;
+        while(i < nums.size()){
+            while(i < nums.size() && nums[i] != 0){
+                i++;
+            }
+            if(i == nums.size()) break;
+            int j = i+1;
+            while(j < nums.size() && nums[j] == 0){
+                j++;
+            }
+            int n = j - i;
+            ans += (1LL)*((1LL)*n*(n+1))/2;
+            i = j + 1;
+        }
+        return ans;
+    }
+
+*/
+
+// Iterative method
+/*
+
+long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans = 0, numSubarray = 0;
+        
+        // Iterate over nums, if num = 0, it has 1 more zero-filled subarray
+        // than the previous one, otherwise, it has 0 zero-filled subarray.
+        for (auto num : nums) {
+            if (num == 0)
+                numSubarray++;
+            else
+                numSubarray = 0;
+            ans += numSubarray;
+        }
+        
+        return ans;
+    }
+
+*/
+
+// Using Hash Map(Couting the different groups of zeros).
+/*
+long long ans = 0;
         unordered_map<int, int> hash;
         int len = 0;
         for(int i = 0; i<nums.size(); i++){
@@ -23,5 +86,5 @@ public:
             }
         }
         return ans;
-    }
-};
+
+*/
