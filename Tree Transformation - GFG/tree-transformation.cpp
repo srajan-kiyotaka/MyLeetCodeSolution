a34629a2eef52bc5d5600225781c0c2256b3a1e3
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-05-22 10:23:57 +0530

    Added solution - LeetHub

--- Tree Transformation - GFG/tree-transformation.cpp
+++ Tree Transformation - GFG/tree-transformation.cpp
@@ -11,6 +11,7 @@ using namespace std;
 class Solution {
     public:
     int solve(int N, vector<int> p){
+        // code here
         set<int> st;
         for(int i=0;i<N;i++){
             if(p[i]!=-1 && p[i]!=0){
@@ -18,8 +19,6 @@ class Solution {
             }
         }
         return st.size();
-        
-        
     }
 };
 
