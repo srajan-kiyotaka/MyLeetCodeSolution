790e8ca986a21a349c78338bd9e92f88b1333fa0
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-05-24 10:04:24 +0530

    Added solution - LeetHub

--- Maximum Identical Bowls - GFG/maximum-identical-bowls.cpp
+++ Maximum Identical Bowls - GFG/maximum-identical-bowls.cpp
@@ -31,7 +31,7 @@ public:
 class Solution {
   public:
     int getMaximum(int N, vector<int> &arr) {
-        long sum = 0;
+       long sum = 0;
         for(int i = 0;i < N;i++)
             sum += arr[i];
         for(int i = N;i > 0;i--)
