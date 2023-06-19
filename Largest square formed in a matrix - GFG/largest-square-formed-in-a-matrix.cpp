a8ea4304c78386f73275196daed964921a3d74d4
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-06-19 19:14:20 +0530

    Added solution - LeetHub

--- Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
+++ Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
@@ -7,27 +7,64 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+// class Solution{
+// public:
+//     int maxSquare(int n, int m, vector<vector<int>> mat){
+//         // DP: Bottom Up Approach: Space Optimization 2.
+//         int maxi = 0;
+//         vector<int> prev(n + 1, 0);
+//         for(int j = m - 1; j >= 0; j--){
+//             vector<int> curr(n + 1, 0);
+//             for(int i = n - 1; i >= 0; i--){
+//                 if(mat[i][j] == 1){
+//                     // move right
+//                     int ans = min(ans, prev[i]);
+//                     // move down
+//                     ans = min(ans, curr[i+1]);
+//                     // move diagonal
+//                     ans = min(ans, prev[i+1]);
+//                     ans++;
+//                     maxi = max(ans, maxi);
+//                     curr[i] = ans;
+//                 } 
+//             }
+//             prev = curr;
+//         }
+//         return maxi;
+//     }
+// };
+
+
 class Solution{
-private:
-    int allSquares(int n, int m, vector<vector<int>>& mat, int row, int col, int &maxi, 
-                   vector<vector<int>> &dp){
-        // Base Case
-        if(row >= n || col >= m) return 0;
-        if(dp[row][col] != -1) return dp[row][col];
-        // Move Right
-        int right = allSquares(n, m, mat, row, col + 1, maxi, dp);
-        // Move Down
-        int down = allSquares(n, m, mat, row + 1, col, maxi, dp);
-        // Move Diagonal
-        int diag = allSquares(n, m, mat, row + 1, col + 1, maxi, dp);
-        // Check
-        if(mat[row][col] == 1){
-            int ans = 1 + min(right, min(down, diag));
-            maxi = max(maxi, ans);
-            return dp[row][col] = ans;
+public:
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        // DP: Bottom Up Approach: Space Optimization 1.
+        int maxi = 0;
+        vector<int> prev(m + 1, 0);
+        for(int i = n - 1; i >= 0; i--){
+            vector<int> curr(m + 1, 0);
+            for(int j = m - 1; j >= 0; j--){
+                if(mat[i][j] == 1){
+                    // move right
+                    int ans = min(ans, curr[j+1]);
+                    // move down
+                    ans = min(ans, prev[j]);
+                    // move diagonal
+                    ans = min(ans, prev[j+1]);
+                    ans++;
+                    maxi = max(ans, maxi);
+                    curr[j] = ans;
+                } 
+            }
+            prev = curr;
         }
-        else return dp[row][col] = 0;
+        return maxi;
     }
+};
+
+/*
+
+class Solution{
 public:
     int maxSquare(int n, int m, vector<vector<int>> mat){
         // DP: Bottom Up Approach: Tabulation.
@@ -52,6 +89,9 @@ public:
         return maxi;
     }
 };
+
+*/
+
 
 /*
 
