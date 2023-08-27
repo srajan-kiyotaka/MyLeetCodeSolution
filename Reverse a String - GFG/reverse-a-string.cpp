0a3c8ae46abb0f5d799eca9830c13fe134a6599f
Srajan Chourasia <91196806+srajan-kiyotaka@users.noreply.github.com>
2023-08-27 21:45:53 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -9,24 +9,33 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+
+#include <string>
+// For the reverse function
+
+
 class Solution
 {
     public:
     string reverseWord(string str)
     {
-         string p;
-        int i=0;
-        stack<char>s;
-        while(i<str.size()){
-            s.push(str[i]);
-            i++;
+       
+        int length= str.length();
+
+       // str.length as the name suggests...checks the length of string.....Example=geeks...length=5 
+       
+         string s="";
+
+        for(int i=length-1;i>=0;i--)        //length-1 because of array serial 4,3,2,1,0
+        {
+            s+=str[i];
+           // ""+s=s...s+k=sk...sk+e=ske...ske+e=skee...skee+g=skeeg
         }
-        while(!s.empty()){
-            p+=s.top();
-            s.pop();
-        }
-        return p;
+        return s;
+       //now s= skeeg  ----->geeks
     }
+
+
 };
 
 
